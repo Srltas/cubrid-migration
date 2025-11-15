cbddd5ccf5964909a227cddfb3b28d716c50b52b
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-11-15 20:32:04 +0900

    Seed metadata session from online source selection

---
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/dbmetadata/session/SourceSchemaSummary.java
@@ -0,0 +1,80 @@
+package com.cubrid.cubridmigration.core.dbmetadata.session;
+
+import java.util.Objects;
+
+/**
+ * Lightweight status holder for a source schema.
+ */
+public class SourceSchemaSummary {
+
+    public enum LoadState {
+        NOT_LOADED,
+        LOADING,
+        LOADED,
+        FAILED
+    }
+
+    private final String schemaName;
+    private boolean selected;
+    private LoadState loadState;
+
+    public SourceSchemaSummary(String schemaName) {
+        this(schemaName, false, LoadState.NOT_LOADED);
+    }
+
+    public SourceSchemaSummary(String schemaName, boolean selected, LoadState loadState) {
+        this.schemaName = schemaName;
+        this.selected = selected;
+        this.loadState = loadState;
+    }
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public LoadState getLoadState() {
+        return loadState;
+    }
+
+    public void setLoadState(LoadState loadState) {
+        this.loadState = loadState;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof SourceSchemaSummary)) {
+            return false;
+        }
+        SourceSchemaSummary other = (SourceSchemaSummary) obj;
+        return Objects.equals(schemaName, other.schemaName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(schemaName);
+    }
+
+    @Override
+    public String toString() {
+        return "SourceSchemaSummary{"
+                + "schemaName='"
+                + schemaName
+                + '\''
+                + ", selected="
+                + selected
+                + ", loadState="
+                + loadState
+                + '}';
+    }
+}
--- plugins/com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/wizard/MigrationWizard.java
+++ plugins/com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/wizard/MigrationWizard.java
@@ -31,12 +31,14 @@
 package com.cubrid.cubridmigration.ui.wizard;
 
 import com.cubrid.common.log.LogUtil;
+import com.cubrid.cubridmigration.core.dbmetadata.session.SourceMetadataSession;
 import com.cubrid.cubridmigration.core.dbobject.Catalog;
 import com.cubrid.cubridmigration.core.dbtype.DatabaseType;
 import com.cubrid.cubridmigration.core.engine.config.MigrationConfiguration;
 import com.cubrid.cubridmigration.core.engine.template.reader.MigrationTemplateReader;
 import com.cubrid.cubridmigration.core.engine.template.writer.MigrationTemplateWriter;
 import com.cubrid.cubridmigration.cubrid.CUBRIDTimeUtil;
+import com.cubrid.cubridmigration.ui.database.provider.SourceMetadataProvider;
 import com.cubrid.cubridmigration.ui.common.UICommonTool;
 import com.cubrid.cubridmigration.ui.common.navigator.event.CubridNodeManager;
 import com.cubrid.cubridmigration.ui.common.navigator.node.DatabaseNode;
@@ -132,6 +134,9 @@ public class MigrationWizard extends Wizard implements IMigrationWizardStatus {
     protected Catalog originalSourceCatalog;
     protected Catalog targetCatalog;
 
+    protected SourceMetadataSession sourceMetadataSession;
+    protected SourceMetadataProvider sourceMetadataProvider;
+
     protected DatabaseNode sourceDBNode;
 
     private boolean saveSchema;
@@ -146,6 +151,7 @@ public class MigrationWizard extends Wizard implements IMigrationWizardStatus {
         migrationConfig = new MigrationConfiguration();
         migrationConfig.setWizardStartDateTime(
                 CUBRIDTimeUtil.wizardStarDateTimeFormat(new Date(System.currentTimeMillis())));
+        sourceMetadataSession = new SourceMetadataSession();
     }
 
     public MigrationWizard(MigrationScript migrationScript) {
@@ -336,6 +342,29 @@ public class MigrationWizard extends Wizard implements IMigrationWizardStatus {
      */
     public Catalog getTargetCatalog() {
         return targetCatalog;
+    }
+
+    public SourceMetadataSession getSourceMetadataSession() {
+        if (sourceMetadataSession == null) {
+            sourceMetadataSession = new SourceMetadataSession();
+        }
+        return sourceMetadataSession;
+    }
+
+    public void setSourceMetadataSession(SourceMetadataSession sourceMetadataSession) {
+        if (sourceMetadataSession == null) {
+            this.sourceMetadataSession = new SourceMetadataSession();
+        } else {
+            this.sourceMetadataSession = sourceMetadataSession;
+        }
+    }
+
+    public SourceMetadataProvider getSourceMetadataProvider() {
+        return sourceMetadataProvider;
+    }
+
+    public void setSourceMetadataProvider(SourceMetadataProvider sourceMetadataProvider) {
+        this.sourceMetadataProvider = sourceMetadataProvider;
     }
 
     public boolean isLoadMigrationScript() {
