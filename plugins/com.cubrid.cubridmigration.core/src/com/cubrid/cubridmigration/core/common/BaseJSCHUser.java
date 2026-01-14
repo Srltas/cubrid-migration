e74aacb1344a4cbc546468abc000f339469e851b
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-01-14 14:25:06 +0900

    Pr test (#81)
    * docs: update application license documentation
    
    * docs: update license files
    
    * chore: update license headers
    
    * chore: update GitHub Action license check answers
    
    * test

--- plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/common/BaseJSCHUser.java
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/common/BaseJSCHUser.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2008 Search Solution Corporation.
- * Copyright (C) 2016 CUBRID Corporation.
+ * Copyright (C) 2016-2026 CUBRID Corporation.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -12,14 +12,14 @@
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
  *
- * - Neither the name of the <ORGANIZATION> nor the names of its contributors
+ * - Neither the name of the copyright holder nor the names of its contributors
  *   may be used to endorse or promote products derived from this software without
  *   specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
  * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
@@ -36,7 +36,7 @@ import com.jcraft.jsch.UserInfo;
  * @author Kevin Cao
  */
 public class BaseJSCHUser implements UserInfo {
-
+//Test
     /**
      * @see com.jcraft.jsch.UserInfo#getPassphrase()
      * @return null;
