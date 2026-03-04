08b40881394f12f4ab8c08c326d3c7f055080f83
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2026-03-05 07:43:30 +0900

    [TOOLS-4840] Improve Tibero Data Export and Migration Core Engine Integration (#374)
    - Connected handlers for INTERVAL DAY TO SECOND, INTERVAL YEAR TO MONTH, JSON, XMLTYPE in handlerMap2 of TiberoExportHelper
    - Extended TiberoDataTypeHelper: added key normalization for JSON and XMLTYPE
    - Unified common text extraction logic for SQLXML, CLOB, and String
    - Added support for JSON binary types (Blob, byte[], InputStream)

---
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/tibero/export/TiberoExportHelper.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) 2016 CUBRID Corporation.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the <ORGANIZATION> nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ */
+package com.cubrid.cubridmigration.tibero.export;
+
+import com.cubrid.common.log.LogUtil;
+import com.cubrid.cubridmigration.core.common.Closer;
+import com.cubrid.cubridmigration.core.connection.ConnParameters;
+import com.cubrid.cubridmigration.core.dbobject.Column;
+import com.cubrid.cubridmigration.core.dbobject.PK;
+import com.cubrid.cubridmigration.core.dbtype.DatabaseType;
+import com.cubrid.cubridmigration.core.engine.config.SourceSequenceConfig;
+import com.cubrid.cubridmigration.core.engine.event.LobMigrationErrorEvent;
+import com.cubrid.cubridmigration.core.export.DBExportHelper;
+import com.cubrid.cubridmigration.core.export.IExportDataHandler;
+import com.cubrid.cubridmigration.core.export.handler.CharTypeHandler;
+import com.cubrid.cubridmigration.core.export.handler.TimestampTypeHandler;
+import com.cubrid.cubridmigration.tibero.TiberoDataTypeHelper;
+import com.cubrid.cubridmigration.tibero.export.handler.TiberoIntervalDSTypeHandler;
+import com.cubrid.cubridmigration.tibero.export.handler.TiberoIntervalYMTypeHandler;
+import com.cubrid.cubridmigration.tibero.export.handler.TiberoJsonTypeHandler;
+import com.cubrid.cubridmigration.tibero.export.handler.TiberoXmlTypeHandler;
+
+import org.slf4j.Logger;
+
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+/** a class help to export Tibero data and verify Tibero sql statement */
+public class TiberoExportHelper extends DBExportHelper {
+    private static final Logger LOG = LogUtil.getLogger(TiberoExportHelper.class);
+
+    /** constructor */
+    public TiberoExportHelper() {
+        super();
+        handlerMap1.put(Types.DATE, new TimestampTypeHandler());
+        handlerMap2.put("INTERVAL DAY TO SECOND", new TiberoIntervalDSTypeHandler());
+        handlerMap2.put("INTERVAL YEAR TO MONTH", new TiberoIntervalYMTypeHandler());
+        handlerMap2.put("JSON", new TiberoJsonTypeHandler());
+        handlerMap2.put("TIMESTAMP WITH LOCAL TIME ZONE", new TimestampTypeHandler());
+        handlerMap2.put("TIMESTAMP WITH TIME ZONE", new CharTypeHandler());
+        handlerMap2.put("XMLTYPE", new TiberoXmlTypeHandler());
+    }
+
+    /**
+     * get JDBC Object
+     *
+     * @param rs ResultSet
+     * @param column Column
+     * @return Object
+     * @throws SQLException e
+     */
+    public Object getJdbcObject(final ResultSet rs, final Column column) throws SQLException {
+        String tibType = TiberoDataTypeHelper.getTiberoDataTypeKey(column.getDataType());
+        IExportDataHandler edh = handlerMap2.get(tibType);
+        try {
+            if (edh != null) {
+                return edh.getJdbcObject(rs, column);
+            }
+            return super.getJdbcObject(rs, column);
+        } catch (SQLException e) {
+            if (column.getDataType().equalsIgnoreCase("BLOB")
+                    || column.getDataType().equalsIgnoreCase("CLOB")) {
+                return new LobMigrationErrorEvent(e);
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * return database object name
+     *
+     * @param objectName String
+     * @return String
+     */
+    protected String getQuotedObjName(String objectName) {
+        return DatabaseType.TIBERO.getSQLHelper(null).getQuotedObjName(objectName);
+    }
+
+    /**
+     * Retrieves the sql with page condition
+     *
+     * @param sql to be change
+     * @param rows per-page
+     * @param exportedRecords start position
+     * @param pk table's primary key
+     * @return SQL
+     */
+    public String getPagedSelectSQL(String sql, long rows, long exportedRecords, PK pk) {
+        String cleanSql = sql.trim();
+        long endRow = exportedRecords + rows;
+        StringBuilder buf = new StringBuilder(cleanSql.length() + 128);
+        buf.append("SELECT * FROM (SELECT CMT_PAGED_.*, ROWNUM CMT_ROWNUM FROM (");
+        buf.append(cleanSql);
+        buf.append(") CMT_PAGED_ WHERE ROWNUM <= ").append(endRow);
+        buf.append(") WHERE CMT_ROWNUM > ").append(exportedRecords);
+        return buf.toString();
+    }
+
+    /**
+     * Retrieves the Database type.
+     *
+     * @return DatabaseType
+     */
+    public DatabaseType getDBType() {
+        return DatabaseType.TIBERO;
+    }
+
+    private static final String SEQUENCE_LAST_NUMBER_SQL =
+            "SELECT S.LAST_NUMBER,S.SEQUENCE_OWNER FROM ALL_SEQUENCES S "
+                    + "WHERE S.SEQUENCE_NAME=? ORDER BY S.SEQUENCE_OWNER";
+
+    /**
+     * Retrieves the current value of input serial.
+     *
+     * @param sourceConParams JDBC connection configuration
+     * @param sq sequence to be synchronized.
+     * @return The current value of the input SQ
+     */
+    public BigInteger getSerialStartValue(ConnParameters sourceConParams, SourceSequenceConfig sq) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = sourceConParams.createConnection();
+            stmt = conn.prepareStatement(SEQUENCE_LAST_NUMBER_SQL);
+            stmt.setString(1, sq.getName());
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                if (sq.getOwner() == null) {
+                    return new BigInteger(rs.getString(1));
+                }
+                if (rs.getString(2).equalsIgnoreCase(sq.getOwner())) {
+                    return new BigInteger(rs.getString(1));
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("TIBERO_SEQUENCE_LAST_NUMBER_SQL", e);
+        } finally {
+            Closer.close(rs);
+            Closer.close(stmt);
+            Closer.close(conn);
+        }
+        return null;
+    }
+}
---
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/tibero/export/handler/AbstractTiberoTextTypeHandler.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2016 CUBRID Corporation.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the <ORGANIZATION> nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ */
+package com.cubrid.cubridmigration.tibero.export.handler;
+
+import com.cubrid.cubridmigration.core.common.Closer;
+import com.cubrid.cubridmigration.core.dbobject.Column;
+import com.cubrid.cubridmigration.core.export.handler.ClobTypeHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLXML;
+
+/** Base handler for Tibero text-oriented source types. */
+public abstract class AbstractTiberoTextTypeHandler extends ClobTypeHandler {
+
+    protected abstract String getTypeNameForError();
+
+    protected Object readTextValue(ResultSet rs, Column column) throws SQLException {
+        final String colName = column.getName();
+        try {
+            return readTextValue(rs.getObject(colName), rs, colName);
+        } catch (Exception e) {
+            if (e instanceof SQLException) {
+                throw (SQLException) e;
+            }
+            throw new SQLException(buildReadErrorMessage(colName), e);
+        }
+    }
+
+    protected Object readTextValue(Object value, ResultSet rs, String colName) throws SQLException {
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof String) {
+            return value;
+        }
+        if (value instanceof SQLXML) {
+            return readFromSqlXml((SQLXML) value);
+        }
+        if (value instanceof Clob) {
+            return getCharObject(((Clob) value).getCharacterStream());
+        }
+        return rs.getString(colName);
+    }
+
+    protected String readFromSqlXml(SQLXML sqlxml) throws SQLException {
+        try {
+            return sqlxml.getString();
+        } finally {
+            try {
+                sqlxml.free();
+            } catch (Exception ignored) {
+                // ignore SQLXML cleanup failure
+            }
+        }
+    }
+
+    protected String readFromBlob(Blob blob, ResultSet rs, String colName) throws SQLException {
+        if (blob == null) {
+            return null;
+        }
+        try {
+            String value = tryGetString(rs, colName);
+            if (value != null) {
+                return value;
+            }
+            return readFromInputStream(blob.getBinaryStream());
+        } finally {
+            try {
+                blob.free();
+            } catch (Exception ignored) {
+                // ignore BLOB cleanup failure
+            }
+        }
+    }
+
+    protected String readFromInputStream(InputStream inputStream) throws SQLException {
+        if (inputStream == null) {
+            return null;
+        }
+        ByteArrayOutputStream out = null;
+        try {
+            out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[2048];
+            int len = inputStream.read(buffer);
+            while (len != -1) {
+                out.write(buffer, 0, len);
+                len = inputStream.read(buffer);
+            }
+            return decodeBinary(out.toByteArray());
+        } catch (Exception e) {
+            throw new SQLException(
+                    "Failed to decode Tibero " + getTypeNameForError() + " binary value", e);
+        } finally {
+            Closer.close(inputStream);
+        }
+    }
+
+    protected String decodeBinary(byte[] bytes) throws SQLException {
+        try {
+            if (bytes == null) {
+                return null;
+            }
+            if (bytes.length == 0) {
+                return "";
+            }
+            CharsetAndOffset detected = resolveCharsetByBom(bytes);
+            return new String(
+                    bytes, detected.offset, bytes.length - detected.offset, detected.charset);
+        } catch (Exception e) {
+            throw new SQLException(
+                    "Failed to decode Tibero " + getTypeNameForError() + " binary value", e);
+        }
+    }
+
+    protected String tryGetString(ResultSet rs, String colName) {
+        try {
+            return rs.getString(colName);
+        } catch (SQLException ignored) {
+            return null;
+        }
+    }
+
+    protected String buildReadErrorMessage(String colName) {
+        return "Failed to read Tibero " + getTypeNameForError() + " value: " + colName;
+    }
+
+    protected CharsetAndOffset resolveCharsetByBom(byte[] bytes) {
+        if (bytes.length >= 3
+                && bytes[0] == (byte) 0xEF
+                && bytes[1] == (byte) 0xBB
+                && bytes[2] == (byte) 0xBF) {
+            return new CharsetAndOffset(StandardCharsets.UTF_8, 3);
+        }
+        if (bytes.length >= 2 && bytes[0] == (byte) 0xFE && bytes[1] == (byte) 0xFF) {
+            return new CharsetAndOffset(StandardCharsets.UTF_16BE, 2);
+        }
+        if (bytes.length >= 2 && bytes[0] == (byte) 0xFF && bytes[1] == (byte) 0xFE) {
+            return new CharsetAndOffset(StandardCharsets.UTF_16LE, 2);
+        }
+        return new CharsetAndOffset(StandardCharsets.UTF_8, 0);
+    }
+
+    protected static final class CharsetAndOffset {
+        private final Charset charset;
+        private final int offset;
+
+        protected CharsetAndOffset(Charset charset, int offset) {
+            this.charset = charset;
+            this.offset = offset;
+        }
+    }
+}
